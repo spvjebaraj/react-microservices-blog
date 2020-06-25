be582e50e003127384c19e4e6750a1048e13b4ba
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-06-25 23:51:51 +0530

    Added client project

---
+++ client/src/components/PostList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPost = async () => {
+    const res = await axios.get("http://posts.com/posts");
+
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+        <CommentList comments={post.comments} />
+        <CommentCreate postId={post.id} />
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
